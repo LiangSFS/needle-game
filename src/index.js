9609acde8df278780c2215f91c6bdce1f7703bdc
liangsfs <1158921160@qq.com>
2019-09-12 17:28:52 +0800

    modify some code specification 规范

--- src/index.js
+++ src/index.js
@@ -4,10 +4,10 @@ import Needle from "./needle.js";
 import "./css/needle.css";
 
 //export default Needle;
-let needle_game = new Needle("#needle_game", {
+let needle_game = new Needle("#needleGame", {
  canvasWidth: 600,
  canvasHeight: 400,
- passNum: 6    //一共有六关
+ passNum: 6,    //一共有六关
  polygonEdgeNum: 6 //六边形
 });
 
