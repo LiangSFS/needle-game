f20fa79580ed65310fcb21d6e2d2accf7042baf5
liangsfs <1158921160@qq.com>
2019-09-13 17:13:14 +0800

    reset latest 9.13

--- config/webpack.utils.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  cssMinimize () {
-     return process.env.NODE_ENV === "production"?true:false;
-  }
-};
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  plugins: {
-    "postcss-preset-env": {
-      "browserslist": ['last 30 versions', "> 2%", "Firefox >=10", "ie 6-11"]
-    }
-  }
-}
